414f84eb74c3260d96ce972c76be4066a6638591
samfelix61 <samfelix61@gmail.com>
2024-03-20 17:05:01 +0300

    complete control flow lab assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+
+  if (ride<= 400) {
+  return "This one is on me!";}
+
+ else if (ride >= 400 && ride <= 2000)
+ {return 'That will be twenty bucks.'}
+
+else if (ride >2000 && ride<= 2500)
+{return 'I will gladly take your thirty bucks.'}
+
+else
+{return 'No can do.'}
+
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(destination){
   // Write your code here!
+
+return destination === "NYC"? "Ok, sounds good.": "No go."  }
+
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+
+switch (tip)
+{
+  case "generous":
+  return 'Thank you so much.'
+   break
+
+  case "not as generous":
+    return 'Thank you.'
+    break
+    default :
+      return 'Bye.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
 }
